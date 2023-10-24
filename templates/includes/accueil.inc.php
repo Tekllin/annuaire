76d47e970191f7052dba2c778494e440a909ee04
tekllin <nathaniel.favario@gmail.com>
2023-10-24 10:04:43 +0200

    modif accueil

--- templates/includes/accueil.inc.php
+++ templates/includes/accueil.inc.php
@@ -1,3 +1,5 @@
 
    <h1>Annuaire</h1>
 
+<?php 
+include "./templates/includes/form.inc.php";
--- templates/includes/menu.inc.php
+++ templates/includes/menu.inc.php
@@ -1,6 +1,5 @@
 
 <ul class="nav">
     <li><a href="./?page=accueil&layout=html">Acceuil</a></li>
-    <li><a href="?page=form" > formulaire d'inscription</a></li>
     <li><a href="?page=listUsers" > inscrits</a></li>
 </ul>
